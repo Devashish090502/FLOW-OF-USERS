53977210ba9610281221e62bc4767b09206f504f
Devashish090502 <128908832+Devashish090502@users.noreply.github.com>
2024-04-25 21:53:07 +0530

    2nd commit

--- checkAssignedUsers.js
+++
@@ -1,6 +0,0 @@
-const users = require('./data/users'); // Import user data
-
-// Loop through users and print assigned astrologer
-users.forEach(user => {
-    console.log(`User ${user.name} is assigned to Astrologer ${user.assignedAstrologer}`);
-});
